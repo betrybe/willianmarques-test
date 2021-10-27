a8a21e352a5403cdcd4d6fc6e2cff9a89313e162
Willian Marques <willian.marques@7care.com.br>
2021-10-27 18:05:23 -0300

    adicionando a model do user com o método create

--- src/api/infra/mongo-helper.js
+++ src/api/infra/mongo-helper.js
@@ -1,16 +1,26 @@
 const { MongoClient } = require('mongodb');
 const { default: mongo } = require('../config/mongo');
 
-export default class MongoHelper {
+class MongoHelper {
     async connect() {
         this.client = await MongoClient.connect(mongo.MONGO_DB_URL, {
           useNewUrlParser: true,
           useUnifiedTopology: true,
         });
-        return this.client.db(mongo.DB_NAME);
+        this.dataBase = this.client.db(mongo.DB_NAME);
     }
 
     async disconnect() {
         await this.client.close();
     }
+
+    async getCollection(name) {
+        if (!this.client.isConnected() || !this.client) {
+          await this.connect();
+        }
+        return this.dataBase.collection(name);
+    }
 }
+
+const mongoHelper = new MongoHelper();
+export default mongoHelper;

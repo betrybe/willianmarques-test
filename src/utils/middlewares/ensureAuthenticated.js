dbd39d4b7e7ad48b4c6e12a9ef3128760c729be5
Willian Marques <willian.marques@7care.com.br>
2021-10-28 09:58:38 -0300

    adicionando middleware de validação de token

---
+++ src/utils/middlewares/ensureAuthenticated.js
@@ -0,0 +1,17 @@
+module.exports = function ensureAuthenticated(req, res, next,){
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error('JWT token is missing.');
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    jwt.verify(token, authConfig.secret);
+
+    return next();
+  } catch {
+    throw new TokenExpiredError('Invalid JWT token');
+  }
+}

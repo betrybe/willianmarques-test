9ac25dae1149a59c47575d40fa4dc4b18208bd43
Willian Marques <willian.marques@7care.com.br>
2021-10-28 09:45:33 -0300

    adicionando recipe model e metodo de criação de recipe

---
+++ src/model/recipe.js
@@ -0,0 +1,12 @@
+module.exports = class User {
+    constructor(dataBaseHelper) {
+        this.dataBaseHelper = dataBaseHelper;
+    }
+
+    async create(name, ingredients, preparation, userId) {
+        const recipesCollection = await this.dataBaseHelper.getCollection('recipes');
+        const recipe = { name, ingredients, preparation, userId };
+        const result = await recipesCollection.insertOne(recipe);
+        return result.ops[0];
+    }
+};
